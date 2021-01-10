5ca3e7a90a198f052fc37e68c95028e6c81882e4
zhangxh1023 <zhangxh1023@qq.com>
2021-01-10 16:58:57 +0800

    chore: code style

--- test/request.test.ts
+++ test/request.test.ts
@@ -2,29 +2,30 @@ import { Request } from '../lib/request';
 import * as http from 'http';
 
 let server: http.Server;
+const serverResponseBody = JSON.stringify({
+  'key': 'value',
+  'array': [1, 2, 3],
+});
 
-beforeAll(() => {
+beforeAll((): Promise<void> => {
   return new Promise(resolve => {
 
     const hostname = '127.0.0.1';
     const port = 3000;
-    
+
     server = http.createServer((req, res) => {
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify({
-        'key': 'value',
-        'array': [1, 2, 3],
-      }));
+      res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+      res.end(serverResponseBody);
     });
-    
+
     server.listen(port, hostname, () => {
       resolve();
     });
   });
 });
 
-afterAll(() => {
+afterAll((): Promise<void> => {
   return new Promise(resolve => {
     server.close(() => {
       resolve();
@@ -36,8 +37,5 @@ it('should request successfully and get the correct response', async () => {
   const { error, response, body } = await Request.get('http://localhost:3000');
   expect(error).toBeNull();
   expect(response.statusCode).toBe(200);
-  expect(body).toBe(JSON.stringify({
-    'key': 'value',
-    'array': [1, 2, 3],
-  }));
+  expect(body).toBe(serverResponseBody);
 });
